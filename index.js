fe5e985a531df16b326b2a1e824f82fbcf0c7eb8
Weltontcheeck <tcheeck2015@gmail.com>
2025-11-02 23:02:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,20 @@ app.use(express.json());
 let escolas = [];
 app.get("/escolas", (req, res) => {
   res.status(200).json(escolas);
+  
 });
 app.post("/escolas", (req, res) => {
-  const { nome, endereco } = req.body;
+     const { nome, endereco } = req.body;
 
-  if (!nome || !endereco) {
-    return res.status(400).json({ erro: "Nome e endereço são obrigatórios." });
-  }
+             if (!nome || !endereco) {
+                 return res.status(400).json({ erro: "Nome e endereço são obrigatórios." });
+                     }
 
-  const novaEscola = {
-    id: escolas.length + 1,
-    nome,
-    endereco
-  };
+                       const novaEscola = {
+                    id: escolas.length + 1,
+                 nome,
+              endereco
+              };
 
   escolas.push(novaEscola);
   res.status(201).json(novaEscola);
@@ -30,4 +31,5 @@ app.post("/escolas", (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor em execução no endereço: http://localhost:${port}`);
 });
+
 
